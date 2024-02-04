c83c2aa39c90b5468842f4d2854f5cd9d215363d
aastha3104 <aastha4502@gmail.com>
2024-02-04 13:17:52 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// var typed= new typed(".text",{
+//     Strings:["Frontend Developer","Web Developer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// })
+// Add this function to your main.js
+
+// main.js
